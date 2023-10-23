232182d0c31955d81f7f69178d57dcfe560a1a12
Jan_Jagoda <jan.jagoda@outlook.com>
2023-10-23 20:34:36 +0200

    Made corrections to Exercise 1

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Exercise 1 - write the getSmallestNumber function using the forEach function
 
 function getSmallestNumber(numbersObject) {
-    const smallestNumber = [];
+    let smallestNumber = numbersObject[0];
     for (let i = 0; i < numbersObject.length; ++i) {
-        if (numbersObject[i] < smallestNumber) {
-            smallestNumber.push(numbersObject[i]);
+        if(numbersObject[i] <= smallestNumber) {
+            smallestNumber = numbersObject[i];
         }
     }
-    return smallestNumber[smallestNumber.length - 1]
+    return smallestNumber;
 }
-console.log(getSmallestNumber([2, -5, 10, 1, 4]));
+console.log(getSmallestNumber([2, 10, -5, 1, 4]));
+console.log(getSmallestNumber([200, 25, 4, 123, 87]));
