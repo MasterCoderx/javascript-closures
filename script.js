14cbc8462fd85914fad2b6e2a96afff642a377bd
Jan_Jagoda <jan.jagoda@outlook.com>
2023-10-26 20:07:11 +0200

    Completed Exercise 6

--- script.js
+++ script.js
@@ -77,3 +77,25 @@ console.log(divideByFive(50)); // 10
 const divideByTwo = getDivideByFunction(2);
 console.log(divideByTwo(8)); // 4
 console.log(divideByTwo(50)); // 25
+
+// Exercise 6 - write the getDeltaFunction
+
+// delta = b*b - 4*a*c
+
+function getDeltaFunction(defaultDeltaNumbersObject) {
+    return function deltaFunction(newDeltaNumbersObject) {
+        const a = newDeltaNumbersObject?.a ?? defaultDeltaNumbersObject.a;
+        const b = newDeltaNumbersObject?.b ?? defaultDeltaNumbersObject.b;
+        const c = newDeltaNumbersObject?.c ?? defaultDeltaNumbersObject.c;
+        return b * b - 4 * a * c;
+    }
+}
+
+const getDelta = getDeltaFunction({ a: 1, b: 2, c: 3 });
+console.log(getDelta()); // -8
+console.log(getDelta({ b: 12 })); // 132
+console.log(getDelta({ a: 4, b: 10 })); // 52
+console.log(getDelta({ a: 4, b: 0 })); // -48
+// The nested function returns NaN if any of the arguments are missing
+console.log(getDeltaFunction({ a: 1, c: 3 })()); // NaN
+console.log(getDeltaFunction({ a: 1, c: 3 })({ b: 15 })); // 213
